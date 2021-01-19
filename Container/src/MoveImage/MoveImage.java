b6af943cac1e4ceb44089c4b149bfeb505a2e853
jHoon0223 <aleejaehun@naver.com>
2021-01-20 01:14:23 +0900

    Move Images through Mouse Pressing

---
+++ Container/src/MoveImage/MoveImage.java
@@ -0,0 +1,46 @@
+package MoveImage;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+
+public class MoveImage extends JFrame {
+
+	JLabel l1;
+	ImageIcon img1;
+	
+	public MoveImage() {
+		setTitle("이미지 넣기");
+		setSize(1080, 720);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		Container pane = getContentPane();
+		pane.setLayout(new FlowLayout());
+		
+		img1 = new ImageIcon("images/SWORD.png");
+		l1 = new JLabel(img1);
+		
+		pane.addMouseListener(new MouseAdapter() {	
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				
+				l1.setLocation(x, y);
+			}		
+		});
+				
+		pane.add(l1);
+		setVisible(true);	
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MoveImage();
+	}
+
+}
