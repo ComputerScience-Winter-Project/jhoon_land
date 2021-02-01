b3b11069fee9ca55179acdf82ef6b70d5c53f6ee
jHoon0223 <aleejaehun@naver.com>
2021-02-01 14:57:19 +0900

    2021_02_01 Implementation of 1st window

--- Container/src/MoveImage/MoveImage.java
+++ Container/src/MoveImage/MoveImage.java
@@ -2,31 +2,34 @@ package MoveImage;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MoveImage extends JFrame {
 
-	JLabel l1;
-	JLabel bg;
-	ImageIcon img1;
-	ImageIcon background;
+	ImageIcon _l1 = new ImageIcon("images/SWORD.png");	//SWORD 이미지 불러오기
+	ImageIcon _bg = new ImageIcon("images/BACK.jpg");	//BACK 배경 불러오기
+	ImageIcon _menu = new ImageIcon("images/menuBar.png");
+	ImageIcon _hbb = new ImageIcon("images/hobbang.gif");
+	JLabel l1 = new JLabel(_l1);	//l1 변수에 SWORD 할당
+	JLabel bg = new JLabel(_bg);	//bg 변수에 BACK 할당
+	JLabel menu = new JLabel(_menu);
+	JLabel hbb = new JLabel(_hbb);
 	
 	public MoveImage() {
-		setTitle("Move Images through Mouse Pressing");	//타이틀
-		setSize(1080, 720);	//창 크기
+		setTitle("Title");	//타이틀
+		setSize(1600, 900);	//창 크기
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//프로그램 종료
 		setLocationRelativeTo(null);	//창 위치 정 가운데에 설정
+		setUndecorated(true);
 		
 		Container pane = getContentPane();
 		pane.setLayout(new FlowLayout());
-		
-		img1 = new ImageIcon("images/SWORD.png");	//SWORD 이미지 불러오기
-		background = new ImageIcon("images/BACK.jpg");	//BACK 배경 불러오기
-		l1 = new JLabel(img1);	//l1 변수에 SWORD 할당
-		bg = new JLabel(background);	//bg 변수에 BACK 할당
 		
 		pane.addMouseListener(new MouseAdapter() {	
 			@Override
@@ -38,8 +41,13 @@ public class MoveImage extends JFrame {
 			}		
 		});
 		
-		pane.add(l1);	//l1 추가
-		pane.add(bg);	//배경 추가
+		hbb.setBounds(0, 0, 128, 128);
+		
+		add(l1);	//l1 추가
+		//pane.add(menu);
+		add(hbb);
+		add(bg);	//배경 추가		
+
 		setVisible(true);	//보이게끔 인자값 true로 설정		
 	}
 	
@@ -47,5 +55,4 @@ public class MoveImage extends JFrame {
 		// TODO Auto-generated method stub
 		new MoveImage();
 	}
-	
 }
