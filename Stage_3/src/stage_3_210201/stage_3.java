a2e889933771b2c8ce639216cddbfb859bdd1f86
jHoon0223 <aleejaehun@naver.com>
2021-03-07 16:36:00 +0900

    210307 : added images and next buttons

--- Stage_3/src/stage_3_210201/stage_3.java
+++ Stage_3/src/stage_3_210201/stage_3.java
@@ -18,30 +18,49 @@ public class stage_3 extends JFrame {
 	private Image screenImage;
 	private Graphics screenGraphic;
 
-	private Image background = new ImageIcon(Main.class.getClassLoader().getResource("images/background.jpg")).getImage();	// 이미지를 담는 객체
+	private Image background = new ImageIcon(Main.class.getClassLoader().getResource("images/BG forest.jpg")).getImage();	// 이미지를 담는 객체
 	
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("buttons/menuBar.png")));
-	private JLabel boss = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/boss.gif")));
 	private JLabel hbb_merong = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/hbb_merong.gif")));
 	private JLabel hbb_sad = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/hbb_sad.gif")));
-	private JLabel ssung = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/ssung.png")));
+	private JLabel hbb_dead = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/hbb_dead.gif")));
+	private JLabel guard = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/guard.png")));
+	private JLabel buy_off = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/buy off.gif")));
+	private JLabel boss = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/boss.gif")));
+	private JLabel boss_atack = new JLabel(new ImageIcon(Main.class.getClassLoader().getResource("images/boss_atack.gif")));
 	
 	private ImageIcon exitButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/exitButton.png"));	
 	private ImageIcon exitButton_hover = new ImageIcon(Main.class.getClassLoader().getResource("buttons/exitButton_hover.png"));
+	private JButton exitButton = new JButton(exitButton_Basic);
 	private ImageIcon moneyButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/moneyButton.png"));
 	private ImageIcon moneyButton_hover = new ImageIcon(Main.class.getClassLoader().getResource("buttons/moneyButton_hover.png"));
+	private JButton moneyButton = new JButton(moneyButton_Basic);
 	private ImageIcon swordButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/swordButton.png"));
 	private ImageIcon swordButton_hover = new ImageIcon(Main.class.getClassLoader().getResource("buttons/swordButton_hover.png"));	
-	private ImageIcon rightButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
-	private ImageIcon leftButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/left.png"));
-	private ImageIcon endingButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/exit.png"));
-	
-	private JButton exitButton = new JButton(exitButton_Basic);
-	private JButton moneyButton = new JButton(moneyButton_Basic);
 	private JButton swordButton = new JButton(swordButton_Basic);
-	private JButton rightButton = new JButton(rightButton_Basic);
-	private JButton leftButton = new JButton(leftButton_Basic);
+	private ImageIcon endingButton_Basic = new ImageIcon(Main.class.getClassLoader().getResource("buttons/exit.png"));
 	private JButton endingButton = new JButton(endingButton_Basic);
+	
+	private ImageIcon next_1 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next1 = new JButton(next_1);
+	private ImageIcon next_2 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next2 = new JButton(next_2);
+	private ImageIcon next_3 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next3 = new JButton(next_3);
+	private ImageIcon next_4 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next4 = new JButton(next_4);
+	private ImageIcon next_5 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next5 = new JButton(next_5);
+	private ImageIcon next_6 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next6 = new JButton(next_6);
+	private ImageIcon next_7 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next7 = new JButton(next_7);
+	private ImageIcon next_8 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next8 = new JButton(next_8);
+	private ImageIcon next_9 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next9 = new JButton(next_9);
+	private ImageIcon next_10 = new ImageIcon(Main.class.getClassLoader().getResource("buttons/right.png"));
+	private JButton next10 = new JButton(next_10);
 	
 	private int mouseX, mouseY;
 
@@ -55,6 +74,8 @@ public class stage_3 extends JFrame {
 		setVisible(true); 				//창 정상적으로 출력
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
+		
+
 		
 		
 		exitButton.setBounds(1250, 5, 20, 20);
@@ -117,16 +138,6 @@ public class stage_3 extends JFrame {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				//칼 버튼 이벤트 처리
-				hbb_merong.setBounds(300, 300, 128, 128);
-				add(hbb_merong);		//칼 버튼 클릭했을때, 호빵이 등장
-				ssung.setBounds(840, 378, 128, 128);
-				add(ssung);
-				add(endingButton);	//엔딩 버튼 삽입
-				background = new ImageIcon(Main.class.getClassLoader().getResource("images/happyending.jpg")).getImage(); //칼 버튼 선택하면 해피엔딩
-				rightButton.setVisible(false);
-				leftButton.setVisible(false);
-				swordButton.setVisible(false);
-				moneyButton.setVisible(false);	//버튼 삭제
 			}
 		});
 		add(swordButton);		//칼 선택 버튼. hover된 상태도 구현 완료
@@ -149,71 +160,219 @@ public class stage_3 extends JFrame {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				//머니 선택 버튼 이벤트 처리
-				boss.setBounds(320, 185, 128, 128);
-				add(boss);		//머니 버튼 선택했을때, 보오스 등장
-				hbb_sad.setBounds(840, 378, 128, 128);
-				add(hbb_sad);
-				add(endingButton);	//엔딩 버튼 삽입
-				background = new ImageIcon(Main.class.getClassLoader().getResource("images/gameover.png")).getImage(); //머니 버튼 선택하면 게임오버
-				rightButton.setVisible(false);
-				leftButton.setVisible(false);				
-				swordButton.setVisible(false);
-				moneyButton.setVisible(false);	//버튼 삭제				
+				
 			}
 		});
 		add(moneyButton);		//머니 선택 버튼. hover된 상태도 구현 완료
 		
-		rightButton.setBounds(1150, 300, 100, 100);
-		rightButton.setBorderPainted(false);
-		rightButton.setContentAreaFilled(false);
-		rightButton.setFocusPainted(false);
-		rightButton.addMouseListener(new MouseAdapter() {
+		next1.setBounds(1150, 300, 100, 100);
+		next1.setBorderPainted(false);
+		next1.setContentAreaFilled(false);
+		next1.setFocusPainted(false);
+		next1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				next1.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				next1.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
-				//right 버튼 이벤트 처리
-				background = new ImageIcon(Main.class.getClassLoader().getResource("images/BG 1.jpg")).getImage();		//배경 교체
-				swordButton.setVisible(false);
-				moneyButton.setVisible(false);
-				hbb_merong.setVisible(false);
-				boss.setVisible(false);		//기존의 버튼과 캐릭터들 삭제
-				add(endingButton);	//엔딩 버튼 삽입
+				//이벤트 처리
 			}
 		});
-		add(rightButton);
+		add(next1);
 		
-		leftButton.setBounds(30, 300, 100, 100);
-		leftButton.setBorderPainted(false);
-		leftButton.setContentAreaFilled(false);
-		leftButton.setFocusPainted(false);
-		leftButton.addMouseListener(new MouseAdapter() {
+		next2.setBounds(1150, 300, 100, 100);
+		next2.setBorderPainted(false);
+		next2.setContentAreaFilled(false);
+		next2.setFocusPainted(false);
+		next2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				next2.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				next2.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
-				//left 버튼 이벤트 처리
-				background = new ImageIcon(Main.class.getClassLoader().getResource("images/BG 2.jpg")).getImage();		//배경 교체
-				swordButton.setVisible(false);
-				moneyButton.setVisible(false);
-				hbb_merong.setVisible(false);
-				boss.setVisible(false);		//기존의 버튼과 캐릭터들 삭제
-				add(endingButton);	//엔딩 버튼 삽입
+				//이벤트 처리
 			}
 		});
-		add(leftButton);	
+		add(next2);
+		
+		next3.setBounds(1150, 300, 100, 100);
+		next3.setBorderPainted(false);
+		next3.setContentAreaFilled(false);
+		next3.setFocusPainted(false);
+		next3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next3.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next3.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next3);
+		
+		next4.setBounds(1150, 300, 100, 100);
+		next4.setBorderPainted(false);
+		next4.setContentAreaFilled(false);
+		next4.setFocusPainted(false);
+		next4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next4.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next4.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next4);
+		
+		next5.setBounds(1150, 300, 100, 100);
+		next5.setBorderPainted(false);
+		next5.setContentAreaFilled(false);
+		next5.setFocusPainted(false);
+		next5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next5.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next5.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next5);
+		
+		next6.setBounds(1150, 300, 100, 100);
+		next6.setBorderPainted(false);
+		next6.setContentAreaFilled(false);
+		next6.setFocusPainted(false);
+		next6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next6.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next6.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next6);
+		
+		next7.setBounds(1150, 300, 100, 100);
+		next7.setBorderPainted(false);
+		next7.setContentAreaFilled(false);
+		next7.setFocusPainted(false);
+		next7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next7.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next7.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next7);
+		
+		next8.setBounds(1150, 300, 100, 100);
+		next8.setBorderPainted(false);
+		next8.setContentAreaFilled(false);
+		next8.setFocusPainted(false);
+		next8.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next8.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next8.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next8);
+		
+		next9.setBounds(1150, 300, 100, 100);
+		next9.setBorderPainted(false);
+		next9.setContentAreaFilled(false);
+		next9.setFocusPainted(false);
+		next9.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next9.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next9.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//이벤트 처리
+			}
+		});
+		add(next9);
+		
+		
+		next10.setBounds(1150, 300, 100, 100);
+		next10.setBorderPainted(false);
+		next10.setContentAreaFilled(false);
+		next10.setFocusPainted(false);
+		next10.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				next10.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				next10.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				boss.setBounds(320, 185, 128, 128);
+				add(boss);		//머니 버튼 선택했을때, 보오스 등장
+				hbb_dead.setBounds(840, 378, 128, 128);
+				add(hbb_dead);
+				add(endingButton);	//엔딩 버튼 삽입
+				background = new ImageIcon(Main.class.getClassLoader().getResource("images/gameover.png")).getImage(); //머니 버튼 선택하면 게임오버		
+				swordButton.setVisible(false);
+				moneyButton.setVisible(false);	//버튼 삭제	
+			}
+		});
+		add(next10);
+		
 		
 
 		menuBar.setBounds(0, 0, 1280, 30);		//메뉴 바 위치 설정
